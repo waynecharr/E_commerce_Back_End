04b99f7710867273ddcdda0e44f3df3d64f9e9c5
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-09-26 16:16:03 -0400

    set up server connection to sequelize and started definding the database models

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     // define columns
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
